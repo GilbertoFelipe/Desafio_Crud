bb5eea844f512e20accdb0dd91e6fd9aa2497e20
Gilberto Felipe <gilbertofelipe.junior@gmail.com>
2024-11-03 10:49:54 -0300

    Desafio_Crud

---
+++ src/main/java/com/devsuperior/desafio_crud/entities/DesafioCrudApplication.java
@@ -0,0 +1,16 @@
+package com.devsuperior.desafio_crud.entities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Locale;
+
+@SpringBootApplication
+public class DesafioCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCrudApplication.class, args);
+		Locale.setDefault(Locale.US);
+	}
+
+}
---
+++ src/main/java/com/devsuperior/desafio_crud/repositories/ClientRepository.java
@@ -0,0 +1,8 @@
+package com.devsuperior.desafio_crud.repositories;
+
+import com.devsuperior.desafio_crud.dto.ClientDTO;
+import com.devsuperior.desafio_crud.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
