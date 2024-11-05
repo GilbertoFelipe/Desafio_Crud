14cacda20bad5545d6a7c4b92b29c8a79da32d65
Gilberto Felipe <gilbertofelipe.junior@gmail.com>
2024-11-05 11:25:22 -0300

    Desafio finalizado

--- src/main/java/com/devsuperior/desafio_crud/controllers/ClientController.java
+++ src/main/java/com/devsuperior/desafio_crud/controllers/ClientController.java
@@ -2,6 +2,7 @@ package com.devsuperior.desafio_crud.controllers;
 
 import com.devsuperior.desafio_crud.dto.ClientDTO;
 import com.devsuperior.desafio_crud.services.ClientService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -33,7 +34,7 @@ public class ClientController {
     }
 
     @PostMapping
-    public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
+    public ResponseEntity<ClientDTO> insert(@Valid @RequestBody ClientDTO dto){
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(dto.getId()).toUri();
@@ -42,7 +43,7 @@ public class ClientController {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto){
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @Valid @RequestBody ClientDTO dto){
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
     }
--- src/main/java/com/devsuperior/desafio_crud/dto/ClientDTO.java
+++ src/main/java/com/devsuperior/desafio_crud/dto/ClientDTO.java
@@ -1,16 +1,30 @@
 package com.devsuperior.desafio_crud.dto;
 
 import com.devsuperior.desafio_crud.entities.Client;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 
 import java.time.LocalDate;
 
 public class ClientDTO {
 
     private Long id;
+
+    @NotBlank(message = "Nome não pode ser vazio")
     private String name;
+
+    @Size(min = 11, max = 11, message = "Deve preencher o campo corretamente")
     private String cpf;
+
+    @Positive(message= "A renda deve ser positiva")
     private Double income;
+
+    @PastOrPresent(message = "Não pode ser data futura")
     private LocalDate birthDate;
+
     private Integer children;
 
     public ClientDTO() {
